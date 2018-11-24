71a72bad3d65ac52a75085f9fe857d1ca0941bc2
markus schulze <markus@masbox001.de>
2018-11-25 00:57:42 +0100

    deleted:  startdocker.sh

--- startdocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mvnproject=module-test
-
-
-echo ${mvnproject}
-
-#docker run -it --rm -v "$PWD":/home/markus/maven-projects/${mvnproject} -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/home/markus/maven-projects/${mvnproject}/target" -w /home/markus/maven-projects/${mvnproject} maven mvn -X clean package >> maven-sysout.txt 2>&1
